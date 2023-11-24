b2314439e32f56c89a7c52109d6706720f30cce0
Hing <128231052+jju9704@users.noreply.github.com>
2023-11-25 02:25:08 +0900

    12.25-2

--- Assets/Script/Summer/Potion.cs
+++ Assets/Script/Summer/Potion.cs
@@ -27,20 +27,45 @@ public class Potion : InteractFunction
     public int CurrentColorIndex = 0;
     public float ColorChangeTime = 2.0f;
 
-    private float CurColorTimer = 0.0f;
-    private Color CurrentColor;
+    private Color CurColor;
     private Color TargetColor;
     private bool IsChanging = false;
+
+    //파티클
+    [Header("Particle System", order = 3), Space(5)]
+    public ParticleSystem particle;
+
 
     #endregion
 
     //Method
     #region .
 
-    //이펙트 수행
-    private void UpdateContentVisuals()
+    //색상 변경
+    private IEnumerator ChangeColor(float changingTime)
     {
-        
+        float timer = changingTime;
+        Color tempColor;
+
+        //색상이 변할 때
+        IsChanging = true;
+        ColorChangeVisuals();
+
+        while (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            tempColor = Color.Lerp(CurColor, TargetColor, 1 - timer / changingTime);
+            Mat.SetColor("_Color", tempColor);
+            yield return null;
+        }
+        CurColor = TargetColor;
+        IsChanging = false;
+    }
+
+    //이펙트 수행
+    private void ColorChangeVisuals()
+    {
+        particle.Play();
     }
 
     #endregion
@@ -48,6 +73,15 @@ public class Potion : InteractFunction
     //Override Method
     #region .
     public override void ToolMainInteract()
+    {
+        if (!IsChanging)
+        {
+            CurrentColorIndex = (CurrentColorIndex + 1) % colors.Length;
+            TargetColor = colors[CurrentColorIndex];
+            StartCoroutine(ChangeColor(ColorChangeTime));
+        }
+    }
+    public override void ToolSubInteract()
     {
         //if (!IsRising)
         //{
@@ -57,14 +91,6 @@ public class Potion : InteractFunction
         //        TargetContentAmount = 0.0f;
         //    }
         //}
-    }
-    public override void ToolSubInteract()
-    {
-        if (!IsChanging)
-        {
-            CurrentColorIndex = (CurrentColorIndex + 1) % colors.Length;
-            TargetColor = colors[CurrentColorIndex];
-        }
     }
     public override void BasicFunction()
     {
@@ -78,60 +104,39 @@ public class Potion : InteractFunction
     // Start is called before the first frame update
     void Start()
     {
-        //내용물 초기화
         Mat = Content.GetComponent<MeshRenderer>().material;
-        CurrentContentAmount = Mat.GetFloat("_Fill");
-        TargetContentAmount = CurrentContentAmount;
+
+        ////내용물 초기화
+        //CurrentContentAmount = Mat.GetFloat("_Fill");
+        //TargetContentAmount = CurrentContentAmount;
 
         //색상 초기화
-        CurrentColor = colors[CurrentColorIndex];
+        CurColor = colors[CurrentColorIndex];
         TargetColor = colors[CurrentColorIndex];
         Mat.SetColor("_Color", TargetColor);
     }
 
     private void Update()
     {
-        //내용물 처리
-        if(CurrentContentAmount != TargetContentAmount)
-        {
-            // 내용물의 양이 변할 때
-            IsRising = true;
+        ////내용물 처리
+        //if(CurrentContentAmount != TargetContentAmount)
+        //{
+        //    // 내용물의 양이 변할 때
+        //    IsRising = true;
 
-            // 내용물의 양 변화
-            CurrentContentAmount = Mathf.MoveTowards(CurrentContentAmount, TargetContentAmount, CotentChangeSpeed * Time.deltaTime);
-            Mat.SetFloat("_Fill", CurrentContentAmount);
+        //    // 내용물의 양 변화
+        //    CurrentContentAmount = Mathf.MoveTowards(CurrentContentAmount, TargetContentAmount, CotentChangeSpeed * Time.deltaTime);
+        //    Mat.SetFloat("_Fill", CurrentContentAmount);
 
-            // 이펙트 처리
-            UpdateContentVisuals();
-        }
-        else
-        {
-            //내용물의 양이 변하지 않을 때
-            IsRising = false;
-            CurrentContentAmount = TargetContentAmount;
-        }
-
-        if(CurrentColor != colors[CurrentColorIndex])
-        {
-            float timer;
-
-            //색상이 변할 때
-            IsChanging = true;
-
-            CurColorTimer += 1 * Time.deltaTime;
-            timer = Mathf.MoveTowards(0, 1, CurColorTimer / ColorChangeTime * Time.deltaTime);
-
-            //색상 처리
-            CurrentColor = Color.Lerp(CurrentColor, TargetColor, timer);
-            Mat.SetColor("_Color", CurrentColor);
-        }
-        else
-        {
-            //색상이 변하지 않을 때
-            IsChanging = false;
-            CurColorTimer = 0.0f;
-            CurrentColor = TargetColor;
-        }
+        //    // 이펙트 처리
+        //    UpdateContentVisuals();
+        //}
+        //else
+        //{
+        //    //내용물의 양이 변하지 않을 때
+        //    IsRising = false;
+        //    CurrentContentAmount = TargetContentAmount;
+        //}
     }
     #endregion
 
