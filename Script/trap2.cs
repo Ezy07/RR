6b158cdaf2090b76c2f6d73c9dc75a0d29733c70
Ezy07 <120762923+Ezy07@users.noreply.github.com>
2023-11-28 11:48:38 +0900

    Add files via upload

---
+++ Script/trap2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap2 : MonoBehaviour
+{
+    public GameObject LiWeapon;
+    public GameObject WaWeapon;
+    public GameObject WiWeapon;
+    public GameObject SnWeapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        WeaponCon WeaponCon = other.GetComponent<WeaponCon>();
+
+        //함정을 지나갈 시 변경
+        if (WeaponCon)
+        {
+            LiWeapon.SetActive(false);
+            WaWeapon.SetActive(false);
+            WiWeapon.SetActive(false);
+            SnWeapon.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
